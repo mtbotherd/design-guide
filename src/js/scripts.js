7268a86f5a67a0efac6c90431102249d95e3722a
mtbotherd <mtbotherd@users.noreply.github.com>
2018-09-06 15:30:25 -0500

    Updates.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -11,23 +11,28 @@ $(document).ready(function() {
         SidebarCollapse();
     });
 
-    function SidebarCollapse() {
-        $('.menu-collapsed').toggleClass('d-none');
-        $('.sidebar-submenu').toggleClass('d-none');
-        $('.submenu-icon').toggleClass('d-none');
-        $('#sidebar-container').toggleClass('sidebar-expanded sidebar-collapsed');
+    // function SidebarCollapse() {
+    //     $('.menu-collapsed').toggleClass('d-none');
+    //     $('.sidebar-submenu').toggleClass('d-none');
+    //     $('.submenu-icon').toggleClass('d-none');
+    //     $('#sidebar-container').toggleClass('sidebar-expanded sidebar-collapsed');
+    //
+    //     // Treating d-flex/d-none on separators with title
+    //     var SeparatorTitle = $('.sidebar-separator-title');
+    //     if (SeparatorTitle.hasClass('d-flex')) {
+    //         SeparatorTitle.removeClass('d-flex');
+    //     } else {
+    //         SeparatorTitle.addClass('d-flex');
+    //     }
+    //
+    //     // Collapse/Expand icon
+    //     $('#collapse-icon').toggleClass('fa-angle-double-left fa-angle-double-right');
+    // }
 
-        // Treating d-flex/d-none on separators with title
-        var SeparatorTitle = $('.sidebar-separator-title');
-        if (SeparatorTitle.hasClass('d-flex')) {
-            SeparatorTitle.removeClass('d-flex');
-        } else {
-            SeparatorTitle.addClass('d-flex');
-        }
+    //Show/hide components - Component Library
+    //$('#mainContent').hide();
 
-        // Collapse/Expand icon
-        $('#collapse-icon').toggleClass('fa-angle-double-left fa-angle-double-right');
-    }
+
 
     // Copy to clipboard - clipboard.js
     //new ClipboardJS('.btn-clipboard ');
