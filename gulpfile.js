15b1604c915af970eb3fccf1887ff3d7326040b6
richbothe <richbothe@gmail.com>
2018-10-24 16:23:57 -0500

    Updates.

--- gulpfile.js
+++ gulpfile.js
@@ -43,6 +43,12 @@ gulp.task('vendorCss', function() {
         .pipe(gulp.dest('dist/css'))
 });
 
+// Copy sourcemaps to dist
+gulp.task('sourcemaps', function() {
+    return gulp.src('src/maps/**/*.map')
+        .pipe(gulp.dest('dist/maps'))
+});
+
 // Compile sass to css
 gulp.task('sass', function() {
     return gulp.src('src/scss/*.scss')
@@ -111,7 +117,7 @@ gulp.task('default', function(callback) {
 gulp.task('build', function(callback) {
     runSequence(
         'clean:dist',
-        'sass', ['useref', 'vendorCss', 'images'],
+        'sass', ['useref', 'vendorCss', 'images', 'sourcemaps'],
         callback
     )
 });
